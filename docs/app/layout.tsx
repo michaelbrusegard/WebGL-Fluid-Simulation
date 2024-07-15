169072669de4e7e5dc3f678fc0e88edecc521fdd
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-07-15 04:49:16 +0200

    docs: setup the pages structure for the new documentation

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -2,14 +2,56 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import type { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
 });
 
+export const metadata: Metadata = {
+  title: 'WebGL Fluid Enhanced',
+  description:
+    'WebGL Fluid Simulation for modern webpages (works even on mobile).',
+  authors: [
+    { name: 'Michael Brusegard', url: 'https://www.michaelbrusegard.com' },
+  ],
+  creator: 'Michael Brusegard',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      url: '/favicon/favicon.ico',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon/favicon-16x16.png',
+    },
+  ],
+  keywords: [
+    'webgl',
+    'background',
+    'navier-strokes',
+    'fluid-simulation',
+    'es-module',
+  ],
+};
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang='en' className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={inter.className} suppressHydrationWarning>
       <body>
         <RootProvider>{children}</RootProvider>
       </body>
